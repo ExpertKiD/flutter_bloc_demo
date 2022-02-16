358427d674cf8742694725d8a1653c63d6d03892
Suman Adhikari <expertxkid@gmail.com>
2022-02-16 12:52:05 +0545

    feat: use MyBlocObserver in main

--- examples/flutter_counter/lib/main.dart
+++ examples/flutter_counter/lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_counter/business_logic/observers/observer.dart';
 import 'package:flutter_counter/presentation/apps/counter_app.dart';
 
 void main() {
-  runApp(const CounterApp());
+  BlocOverrides.runZoned(() => runApp(const CounterApp()),
+      blocObserver: kDebugMode ? MyBlocObserver() : null);
 }
