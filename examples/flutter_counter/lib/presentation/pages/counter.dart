846d53ed699aff0b9319e237f67dd75f3e9c7643
Suman Adhikari <expertxkid@gmail.com>
2022-02-16 13:39:02 +0545

    feat: add counter page

--- examples/flutter_counter/lib/presentation/pages/counter.dart
+++ examples/flutter_counter/lib/presentation/pages/counter.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_counter/business_logic/cubits/counter_cubit.dart';
+
+import '../widgets/counter.dart';
 
 class CounterPage extends StatelessWidget {
   const CounterPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return BlocProvider<CounterCubit>(
+      create: (_) => CounterCubit(),
+      child: const CounterView(),
+    );
   }
 }
