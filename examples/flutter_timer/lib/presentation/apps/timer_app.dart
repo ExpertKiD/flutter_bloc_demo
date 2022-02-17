0f8c6bd10e0f8fa2b5f5a62574789cfbaa76b07f
Suman Adhikari <expertxkid@gmail.com>
2022-02-17 11:39:55 +0545

    refactor: add theme

--- examples/flutter_timer/lib/presentation/apps/timer_app.dart
+++ examples/flutter_timer/lib/presentation/apps/timer_app.dart
@@ -7,8 +7,14 @@ class TimerApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Timer',
+      theme: ThemeData(
+        primaryColor: const Color.fromRGBO(109, 234, 255, 1),
+        colorScheme: const ColorScheme.light(
+          secondary: Color.fromRGBO(72, 74, 126, 1),
+        ),
+      ),
       onGenerateRoute: AppRouter.onGenerateRoute,
     );
   }
