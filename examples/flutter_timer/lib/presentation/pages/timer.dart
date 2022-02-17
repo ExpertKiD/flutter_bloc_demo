1fc3ee14c4580a77771139dbd7878a353b98fade
Suman Adhikari <expertxkid@gmail.com>
2022-02-17 12:01:30 +0545

    refactor: update imports

--- examples/flutter_timer/lib/presentation/pages/timer.dart
+++ examples/flutter_timer/lib/presentation/pages/timer.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_timer/business_logic/blocs/timer_bloc.dart';
 import 'package:flutter_timer/data/repositories/ticker.dart';
-import 'package:flutter_timer/presentation/widgets/timer/timer_view.dart';
+import 'package:flutter_timer/presentation/widgets/timer/timer.dart';
+
+import '../widgets/timer/timer.dart';
 
 class TimerPage extends StatelessWidget {
   const TimerPage({Key? key}) : super(key: key);
