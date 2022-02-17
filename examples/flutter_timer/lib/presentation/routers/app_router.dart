36e3647e0d27baff2e917a339163c13859f7e683
Suman Adhikari <expertxkid@gmail.com>
2022-02-17 11:39:29 +0545

    refactor: set to view TimerPage on home

--- examples/flutter_timer/lib/presentation/routers/app_router.dart
+++ examples/flutter_timer/lib/presentation/routers/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_timer/presentation/pages/timer.dart';
 
 import '../routes/routes.dart';
 
@@ -7,7 +8,7 @@ class AppRouter {
   static Route<dynamic>? onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
       case Routes.home:
-        return MaterialPageRoute(builder: (_) => const Scaffold());
+        return MaterialPageRoute(builder: (_) => const TimerPage());
 
       default:
         return null;
